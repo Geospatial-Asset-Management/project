ae829f550765ece41e2b99e250fe89cb5bea008d
bbbayazitt <berkay01907@gmail.com>
2021-04-02 17:35:41 +0300

    user uygulamasi eklendi

--- asset/settings.py
+++ asset/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'schema_graph',
-    'django-dia'
+    "user",
 ]
 
 MIDDLEWARE = [
---
+++ user/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render,redirect
+from  .forms import RegisterForm
+from .forms import LoginForm
+
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate
+from django.contrib import messages
+
+# Create your views here.
+
+def register (request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+
+            newUser = User(username =username)
+            newUser.set_password(password)
+
+            newUser.save()
+            login(request,newUser)
+            messages.success(request,"Basariyla kayit oldunuz")
+
+            return redirect("index")
+        context = {
+            "form" : form
+        }
+        return render(request,"register.html",context)
+
+
+    else:
+        form = RegisterForm()
+        context = {
+            "form" : form
+        }
+        return render(request,"register.html",context)
+
+
+
+def loginUser (request):
+    form = LoginForm(request.POST or None)
+
+    context = {
+        "form": form
+    }
+
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        user = authenticate(username=username,password=password)
+
+        if user is None:
+            messages.info(request,"kullanici adi veya parola hatali")
+            return render(request,"login.html",context)
+
+        messages.success(request,"basariyla giris yaptiniz")
+        login(request,user)
+        return redirect("index")
+    return render(request,"login.html",context)
+
+
+def logoutUser (request):
+    return render(request,"logout.html")
+
+def index(request):
+    return render(request,"index.html",context={"myvar":"Deneme"})
