ecc837f223061b5420ed51b30ba76792220c4b19
bbbayazitt <berkay01907@gmail.com>
2021-04-02 18:21:49 +0300

    son

--- asset/urls.py
+++ asset/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'), #view.py def adı
     path('home/', home_view, name='home'), #üsttekinin aynısını böyle yazınca http://127.0.0.1:8000/home adresinde üsttekinin aynısı çıkacak
-    path('assets/', cesiumAsset)
+    path('assets/', cesiumAsset),
+    path('user/',include("user.urls")),
 
 ]
